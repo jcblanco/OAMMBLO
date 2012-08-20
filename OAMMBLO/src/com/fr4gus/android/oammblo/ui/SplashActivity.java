cc785bc7d752fdb6f75192a97181cc6a0dc2fa26
Juan Carlos Blanco C <juancablanco88@gmail.com>
2012-08-19 19:20:29 -0600

    Tarea 1 terminada

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
@@ -1,21 +1,38 @@
 package com.fr4gus.android.oammblo.ui;
 
+import com.fr4gus.android.oammblo.LoginActivity;
 import com.fr4gus.android.oammblo.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Shows App logo for few seconds.
  * 
- * @author Franklin Garcia
- * Created Mar 25, 2012
+ * @author Franklin Garcia Created Mar 25, 2012
  */
 public class SplashActivity extends OammbloActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-    }
+	Handler mHandler;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		mHandler = new Handler();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mHandler.postDelayed(new Runnable() {
+			public void run() {
+				startActivity(new Intent(SplashActivity.this,
+						LoginActivity.class));
+			}
+		}, 3000);
+	}
 
 }
